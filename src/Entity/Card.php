5eceb2097017738ad7858f79a02ab4334ef19579
Juan González <juangonzalezmontes@gmail.com>
2021-08-05 22:35:35 +0200

    feature/011-many-to-many-bidirectional
    - Add bidirectionality to Card with User

--- src/Entity/Card.php
+++ src/Entity/Card.php
@@ -2,17 +2,21 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Symfony\Component\Uid\Uuid;
 
 class Card
 {
     private string $id;
     private string $value;
+    private Collection $users;
 
     public function __construct(string $value)
     {
         $this->id = Uuid::v4()->toRfc4122();
         $this->value = $value;
+        $this->users = new ArrayCollection();
     }
 
     public function getId(): string
@@ -25,11 +29,22 @@ class Card
         return $this->value;
     }
 
+    public function getUsers(): ArrayCollection|Collection
+    {
+        return $this->users;
+    }
+
     public function toArray(): array
     {
         return [
             'id' => $this->id,
             'value' => $this->value,
+            'users' => array_map(function (User $user): array {
+                return [
+                    'id' => $user->getId(),
+                    'name' => $user->getName(),
+                ];
+            }, $this->users->toArray())
         ];
     }
 }
